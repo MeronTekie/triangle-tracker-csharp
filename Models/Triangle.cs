1ad13896cd3519776a61addd405c131b1e29b392
Marcus <marcus.kaitara.lorenzo@gmail.com>
2022-05-03 10:25:15 -0700

    init Triangle Tracker
    Co-authored-by: Meron G. Tekie <MeronTekie@users.noreply.github.com>

---
+++ Models/Triangle.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleProject
+{
+    public class Triangle
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hi, enter a string below")  ;
+            // int Side1= ""
+            string input = Console.ReadLine();
+            Console.WriteLine(input);
+        }
+    }
+}
