f398243b140641024f0029ade6fbbc6654665730
Marcus <marcus.kaitara.lorenzo@gmail.com>
2022-05-03 11:06:24 -0700

    finish tests
    Co-authored-by: Meron G. Tekie <MeronTekie@users.noreply.github.com>

--- Models/Triangle.cs
+++ Models/Triangle.cs
@@ -8,21 +8,25 @@ namespace Triangles
 {
     public class Triangle
     {
-        public bool IsItTriangle(int Side1,int Side2,int Side3)
+        public string IsItTriangle(int Side1,int Side2,int Side3)
         {
             if(Side1 +Side2 > Side3  && Side2 +Side3 > Side1 && Side1 +Side3 > Side2)
             {
-                if( Side1 ==Side2 ||Side1 ==Side3||Side3 ==Side2)
+                    if(Side1 ==Side2 &&Side1==Side3)
                 {
-                    return true; 
-                } 
+                    return "Equilateral";
+                }
+                    else if( Side1 ==Side2 ||Side1 ==Side3||Side3 ==Side2)
+                {
+                    return "Isoceles"; 
+                }                
                 else 
                 {
-                    return false;
+                    return "Scalene";
                 }    
             }
         
-            return false;
+            return "Not a Triangle";
             
 
         }
