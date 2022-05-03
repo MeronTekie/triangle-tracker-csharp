da75cf35502a79eb92d917c676ddd9891b6f1424
Marcus <marcus.kaitara.lorenzo@gmail.com>
2022-05-03 10:54:08 -0700

    red green first tests
    Co-authored-by: Meron G. Tekie <MeronTekie@users.noreply.github.com>

--- Models/Triangle.cs
+++ Models/Triangle.cs
@@ -4,16 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TriangleProject
+namespace Triangles
 {
     public class Triangle
     {
-        static void Main()
+        public bool IsItTriangle(int Side1,int Side2,int Side3)
         {
-            Console.WriteLine("Hi, enter a string below")  ;
-            // int Side1= ""
-            string input = Console.ReadLine();
-            Console.WriteLine(input);
+            if(Side1 +Side2 > Side3  && Side2 +Side3 > Side1 && Side1 +Side3 > Side2)
+            {
+                if( Side1 ==Side2 ||Side1 ==Side3||Side3 ==Side2)
+                {
+                    return true; 
+                } 
+                else 
+                {
+                    return false;
+                }    
+            }
+        
+            return false;
+            
+
         }
+        // static void Main()
+        // {
+        //     Console.WriteLine("Hi, enter a string below")  ;
+        //     // int Side1= ""
+        //     string input = Console.ReadLine();
+        //     Console.WriteLine(input);
+        // }
     }
 }
